a23605bcd4e1383ac14b3ef998af3987e0372165
Gabriel <gabrieljapadantas@gmail.com>
2023-10-28 00:37:21 -0300

    Atualização do DB

--- src/SQL/dump.sql
+++ src/SQL/dump.sql
@@ -18,3 +18,25 @@ categorias (descricao)
 VALUES 
 ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), 
 ('Livros e Papelaria'), ('Brinquedos'), ('Moda'), ('Bebê'), ('Games');
+
+
+CREATE TABLE produtos(
+  id SERIAL PRIMARY KEY UNIQUE,
+  descricao TEXT NOT NULL UNIQUE,
+  quantidade_estoque INTEGER NOT NULL,
+  valor INTEGER NOT NULL,
+  categoria_id INTEGER NOT NULL REFERENCES categorias (id)
+);
+
+CREATE TABLE clientes(
+id SERIAL PRIMARY KEY UNIQUE,
+nome text NOT NULL,
+email TEXT  NOT NULL UNIQUE,
+cpf CHAR(11) NOT NULL UNIQUE,
+cep INTEGER,
+rua TEXT,
+numero INTEGER,
+bairro TEXT,
+cidade TEXT,
+estado CHAR(2)
+);
